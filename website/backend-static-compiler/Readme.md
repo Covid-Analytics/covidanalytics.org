ab85191dbd5b714af835e64663e598dd2ccc6c3e
Enrico Ros <enrico.ros@gmail.com>
2020-03-25 16:38:41 -0700

    website/backend: build script that does the ipython to index.html conversion

--- website/backend-static-compiler/Readme.md
+++ website/backend-static-compiler/Readme.md
@@ -2,4 +2,8 @@ Using a Docker container to encapsulate the "static building" of notebooks into 
 
 The command we'll run need to have the full repo as build context, so we'll converge to something like:
 
-  docker build ../../ -f Dockerfile --tag=ca-backend-compiler
+  ```docker build ../../ -f Dockerfile --tag=covana-backend-compiler```
+
+To test the container:
+
+  ```docker run --rm -it covana-backend-compiler /bin/bash```
--- website/backend-static-compiler/build-all-notebooks.sh
+++ website/backend-static-compiler/build-all-notebooks.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
+
+# change this to specify where this application will be served from
+# it not in a parameter yet for risk management
+INSTALL_DIR="/srv/org.covidanalytics/static"
+
+# build the container to statically compiler, and compile the notebooks to html
 docker build ../../ -f Dockerfile --tag=covana-backend-compiler
+
+# compile each notebook by allocating and running a compiler container
+for Notebook in ../../analysis/*.ipynb; do
+  echo "Creating container to compiler $Notebok..."
+  Compiler=`docker run -d --rm -t covana-backend-compiler:latest /bin/bash`
+  echo " > copying over the notebook"
+  docker cp "$Notebook" $Compiler:/app/notebook.ipynb
+  echo " > compiling it"
+  docker exec $Compiler jupyter-nbconvert --to html --template full --execute /app/notebook.ipynb
+  echo " > getting the result back"
+  docker cp $Compiler:/app/notebook.html "$INSTALL_DIR/index.html"
+  echo " > killing the compiler container."
+  docker kill $Compiler > /dev/null
+done
