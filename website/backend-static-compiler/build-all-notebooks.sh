b12afb2ec401e723b453c13ccc17e46323ef0373
Enrico Ros <enrico.ros@gmail.com>
2020-03-26 22:03:54 -0700

    website/backend: docker adapted

--- website/backend-static-compiler/build-all-notebooks.sh
+++ website/backend-static-compiler/build-all-notebooks.sh
@@ -6,7 +6,7 @@ INSTALL_DIR="/srv/org.covidanalytics/static"
 
 # build the container to statically compile the notebooks to html
 echo "Building or refreshing the Docker container for compiling notebooks to HTML"
-docker build . -f Dockerfile --tag=covana-backend-compiler 2> /dev/null
+docker build . -f Dockerfile --tag=covana-backend-compiler
 echo
 
 # compile each notebook by allocating and running a compiler container
@@ -14,7 +14,7 @@ for Notebook in ../../analysis/*.ipynb; do
   echo "[`date`] Using the 'covana-backend-compiler' container to convert: $Notebook"
   Compiler=`docker run -d --rm -t covana-backend-compiler:latest /bin/bash`
   docker cp "$Notebook" $Compiler:/app/notebook.ipynb
-  docker exec $Compiler python3 /app/convert-ipynb.py
+  docker exec -t $Compiler python3 /app/convert-ipynb.py
   #docker exec -it $Compiler /bin/bash
   docker cp $Compiler:/app/notebook.html "$INSTALL_DIR/index.html"
   docker kill $Compiler > /dev/null
