8ef72a8aabf6e02984705ad7c2127bf79e82cb03
Enrico Ros <enrico.ros@gmail.com>
2020-03-29 16:27:29 -0700

    website/frontend: improve build script

--- website/build-website.sh
+++ website/build-website.sh
@@ -11,14 +11,18 @@ LOCAL_FRONTEND_OUTPUT="out_frontend"
 # build the container to statically compile the notebooks to html
 docker build . -f Dockerfile.converter --tag=covana-converter
 
-# perform the conversion (instanciate the container, copy files, excute script, copy back output)
+# perform the conversion (instanciate the container, copy files, excute script, copy back output) - 1 minute
 # NOTE: to inspect the image at any state: docker exec -it $CONVERTER_PROCESS /bin/bash
+echo "> Start Notebooks Converter container"
 CONVERTER_PROCESS=$(docker run --rm -d -t covana-converter:latest /bin/bash)
+
 echo "> Copying notebooks from '../../../analysis' to the container 'input/' folder"
 for NOTEBOOK in ../analysis/*.ipynb; do docker cp "$NOTEBOOK" "$CONVERTER_PROCESS":/app/input/; done
+
 echo "> Converting Notebooks (and copying the output to $LOCAL_CONVERTER_OUTPUT)..."
-docker exec -t "$CONVERTER_PROCESS" python3 /app/convert-ipynb.py
+time docker exec -t "$CONVERTER_PROCESS" python3 /app/convert-ipynb.py
 docker cp "$CONVERTER_PROCESS":/app/output/ "$LOCAL_CONVERTER_OUTPUT"
+
 echo "> Removing container..."
 docker kill "$CONVERTER_PROCESS" > /dev/null
 echo "...done."
@@ -27,13 +31,19 @@ echo "...done."
 
 # build the container
 docker build . -f Dockerfile.frontend --tag=covana-frontend
+
+# perform the frontend compilation - 2 minutes
+echo "> Start Frontend compiler container"
 FRONTEND_PROCESS=$(docker run --rm -d -t covana-frontend:latest /bin/bash)
+
 echo "> Copying GLUE files (site already in the image)..."
 # TODO: IMPROVE GLUE
 docker cp "$LOCAL_CONVERTER_OUTPUT/." "$FRONTEND_PROCESS":/app/public/
+
 echo "> Compiling Frontend (and copying the output to $LOCAL_FRONTEND_OUTPUT)..."
-docker exec -t "$FRONTEND_PROCESS" npm run build
+time docker exec -t "$FRONTEND_PROCESS" npm run build
 docker cp "$FRONTEND_PROCESS":/app/build/ "$LOCAL_FRONTEND_OUTPUT"
+
 echo "> Removing container..."
 docker kill "$FRONTEND_PROCESS" > /dev/null
 echo "...done."
