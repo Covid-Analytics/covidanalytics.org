274f6c6d5281f2c8c4eb11c0401337b8fd0658d9
Enrico Ros <enrico.ros@gmail.com>
2020-03-29 17:01:44 -0700

    frontend: remove RTL

--- website/frontend/src/index.js
+++ website/frontend/src/index.js
@@ -20,8 +20,7 @@ import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 import AuthLayout from "layouts/Auth.js";
-import RtlLayout from "layouts/RTL.js";
-import AdminLayout from "layouts/Admin.js";
+import AdminLayout from "layouts/DashboardLayout.js";
 
 import "assets/scss/material-dashboard-pro-react.scss?v=1.8.0";
 
@@ -30,7 +29,6 @@ const hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/rtl" component={RtlLayout} />
       <Route path="/auth" component={AuthLayout} />
       <Route path="/admin" component={AdminLayout} />
       <Redirect from="/" to="/admin/dashboard" />
