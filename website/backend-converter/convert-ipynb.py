4f331522fef7bcf92a123cd715c37536fc47a685
Enrico Ros <enrico.ros@gmail.com>
2020-03-29 19:52:03 -0700

    website/converter: improve page export/2

--- website/backend-converter/convert-ipynb.py
+++ website/backend-converter/convert-ipynb.py
@@ -15,7 +15,7 @@ NOTEBOOK_PATHS = ['.', './input', '../../analysis']
 OUTPUT_FOLDER = 'output'
 
 # This is the inline'd template
-inline_template = DictLoader({'ours.tpl': """
+stand_alone_tpl = """
 {%- extends 'full.tpl' -%}
 
 {% block input_group %}
@@ -36,7 +36,24 @@ inline_template = DictLoader({'ours.tpl': """
         }
     </style>
 {% endblock html_head %}
-"""})
+"""
+react_glue_tpl = """
+{%- extends 'basic.tpl' -%}
+
+{%- block header -%}
+module.exports = function(props) {
+  return (
+{%- endblock header -%}
+
+{% block body %}
+    {{ super() }}
+{%- endblock body %}
+
+{% block footer %}
+  );
+};
+{% endblock footer %}
+"""
 
 
 def input_file_to_folder(basename):
@@ -69,12 +86,12 @@ def convert_notebook_to_assets(notebook_file_name, base_name, output_prefix):
     nb = nbformat.reads(nb_file, as_version=4)
 
     # 1. clear output
-    print(" - clearing output...")
+    print(" - clearing output")
     ep = ClearOutputPreprocessor()
     ep.preprocess(nb, {})
 
-    # 2. generate new outputs
-    print(" - executing...")
+    # 2. generate fresh charts by running the notebook
+    print(" - executing")
     ep = ExecutePreprocessor(timeout=600, kernel_name='python3', allow_errors=False)
     try:
         ep.preprocess(nb, {'metadata': {'path': output_folder}})
@@ -84,51 +101,57 @@ def convert_notebook_to_assets(notebook_file_name, base_name, output_prefix):
         print(e)
         exit(1)
 
-    # 3. ...
-    # save pics?
-
-    # 4. export to HTML using the current template engine
-    print(" - generating html...")
-    c_clean = Config({
-        "TemplateExporter": {
+    # 3. export HTML
+    print(" - generating html")
+    cleaner_config = Config({
+        "HTMLExporter": {
             "exclude_input": True,
             "exclude_input_prompt": True,
             "exclude_output_prompt": True,
-        },
-        "HTMLExporter": {
             "preprocessors": ['nbconvert.preprocessors.ExtractOutputPreprocessor']
         },
     })
-    html_exporter = HTMLExporter(config=c_clean, extra_loaders=[inline_template])
-    html_exporter.template_file = 'ours.tpl'
-    (body, resources) = html_exporter.from_notebook_node(nb)
+    local_templates = DictLoader({
+        'our-html.tpl': stand_alone_tpl,
+        'react-glue.tpl': react_glue_tpl,
+    })
+    exporter = HTMLExporter(config=cleaner_config, extra_loaders=[local_templates])
+    exporter.template_file = 'our-html.tpl'
+    (html_body, html_resources) = exporter.from_notebook_node(nb)
 
     # save html output file, with local reference to the pictures
     local_html = []
     output_html_file_name = output_folder + '/' + "index.html"
-    print(" - saving html to file: " + output_html_file_name)
+    print("   - saving html: " + output_html_file_name)
     with open(output_html_file_name, 'wt') as the_file:
-        the_file.write(body)
+        the_file.write(html_body)
     local_html.append({
         'notebook': base_name,
         'html_notebook': output_html_file_name,
-        'html_body': body,
     })
 
+    # save js file for react inclusion (local ref to the pictures)
+    # exporter.template_file = 'react-glue.tpl'
+    # (react_body, react_resources) = exporter.from_notebook_node(nb)
+    # output_react_file_name = output_folder + '/' + "index.js"
+    # print("   - saving react js: " + output_react_file_name)
+    # with open(output_react_file_name, 'wt') as the_file:
+    #     the_file.write(react_body)
+
     # save all the figures
-    local_assets = []
-    figures = resources['outputs']
+    local_figures = []
+    figures = html_resources['outputs']
     figures_count = len(figures)
     figure_index = 1
     for figure_file in figures:
         output_figure_file_name = output_folder + '/' + figure_file
-        print(" - saving figure " + str(figure_index) + " of " + str(figures_count) + ": " + output_figure_file_name)
+        print("   - saving png " + str(figure_index) + " of " + str(figures_count) + ": " + output_figure_file_name)
         if not figure_file.endswith('.png'):
             print("WARNING: figure is not a PNG file")
             continue
         with open(output_figure_file_name, 'wb') as the_file:
             the_file.write(figures[figure_file])
-        local_assets.append({
+        local_figures.append({
             'notebook': base_name,
             'html_notebook': output_html_file_name,
             'figure': figure_file,
@@ -141,7 +164,7 @@ def convert_notebook_to_assets(notebook_file_name, base_name, output_prefix):
         the_file.write("")
 
     # return a recap of all assets
-    return local_html, local_assets
+    return local_html, local_figures
 
 
 def write_assets_loader(pages, assets, output_prefix):
@@ -155,9 +178,9 @@ notebooks = scan_for_notebooks(NOTEBOOK_PATHS)
 all_pages = []
 all_figures = []
 for task in notebooks:
-    nb_html, nb_assets = convert_notebook_to_assets(task['input'], task['basename'], OUTPUT_FOLDER)
+    nb_html, nb_figures = convert_notebook_to_assets(task['input'], task['basename'], OUTPUT_FOLDER)
     all_pages.extend(nb_html)
-    all_figures.extend(nb_assets)
+    all_figures.extend(nb_figures)
 write_assets_loader(all_pages, all_figures, OUTPUT_FOLDER)
 
 print("done.")
