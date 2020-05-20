48a2a6596c9952a2d6ec2243065fbc76c4f25872
Enrico Ros <enrico.ros@gmail.com>
2020-05-19 18:22:54 -0700

    Update every 2h, not 5m. ~1/20th load

--- website/build-continuous-loop.sh
+++ website/build-continuous-loop.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 while /bin/true; do
-  echo "[$(date)] Running"
+  echo "[$(date)] Running - Next update in 2h"
   git pull
   echo
   ./build-website.sh
-  sleep 5m;
+  sleep 2h;
+#  sleep 5m;
   echo
 done
