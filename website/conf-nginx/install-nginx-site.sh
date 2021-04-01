8c76113a0f6cfabb033aee3de83c6ae12d8872ba
Enrico Ros <enrico.ros@gmail.com>
2021-04-01 01:24:03 -0700

    website: fix domain

--- website/conf-nginx/install-nginx-site.sh
+++ website/conf-nginx/install-nginx-site.sh
@@ -2,7 +2,7 @@
 
 # First manual setup: generation of the SSH keys for the domain.
 # The following has been executed manually.
-#  certbot certonly --nginx --expand  -d www.covidanalytics.com -d covidanalytics.com
+#  certbot certonly --nginx --expand  -d www.covidanalytics.org -d covidanalytics.org
 
 # copy the frontend configuration (note: this requires the SSH certs to be present, as per the above)
 SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
